c25415b9c91a263d870d0245937820cb726e4b08
Adrien Haxaire <adrien@haxaire.org>
2020-11-23 22:04:31 +0100

    Job 1 succeeds

--- applications/jobs-flow/job1.go
+++ applications/jobs-flow/job1.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 )
 
 func main() {
 	message := []byte("Writing from job1")
-	err := ioutil.WriteFile("data/jobs.txt", message, 0644)
+	err := ioutil.WriteFile("/app/data/jobs.txt", message, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("Final message: %s", message)
 }
--- applications/jobs-flow/job2.go
+++ applications/jobs-flow/job2.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 )
 
 func main() {
-	content, err := ioutil.ReadFile("data/jobs.txt")
+	content, err := ioutil.ReadFile("/app/data/jobs.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	upd := append(content, []byte(" and from job2")...)
-	err = ioutil.WriteFile("data/jobs.txt", upd, 0644)
+	err = ioutil.WriteFile("/app/data/jobs.txt", upd, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("Final message: %s", upd)
 }
